18a70609dac78e506f39118f265da1e18fb65c98
zero <rpslzero0@gmail.com>
2025-07-27 00:03:39 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -51,7 +51,7 @@ chmod +x /app/rest_api
 chmod +x /app/stark_verify
 
 echo "-----Installing CLI tools-----"
-git clone https://github.com/fadhilahkholiq/boundless.git
+git clone https://github.com/Stevesv1/boundless.git
 cd ~/boundless
 git submodule update --init --recursive
 cargo install --locked --git https://github.com/risc0/risc0 bento-client --bin bento_cli
